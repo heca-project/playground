14e77f03e8db599ac6f8492172e1352a8a462a8e
I picked a name <ipickedaname@mail.com>
2020-01-02 19:42:58 -0800

    bump to 0.3.0

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -8,7 +8,7 @@ strip "/tmp/heca/$TARGET/release/playground"
 name="playground-${TRAVIS_TAG}-${TARGET}"
 mkdir -p /tmp/heca-staging-1
 mkdir -p /tmp/heca-staging
-cp "/tmp/heca/$TARGET/release/heca" "/tmp/heca-staging-1/$name"
+cp "/tmp/heca/$TARGET/release/playground" "/tmp/heca-staging-1/$name"
 xz "/tmp/heca-staging-1/$name"
 cp "/tmp/heca-staging-1/$name.xz" "/tmp/heca-staging/"
 cp "/tmp/heca/$TARGET/release/playground" "/tmp/heca-staging/$name"
